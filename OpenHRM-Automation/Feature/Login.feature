2086e0869a744d251670dfaf94c2715bd7f37835
EAD\6124529 <pranayrahate16@gmail.com>
2024-09-27 10:46:59 +0530

    Add project files.

---
+++ OpenHRM-Automation/Feature/Login.feature
@@ -0,0 +1,12 @@
+﻿Feature: Login
+
+Checking the Login feature with valid creds
+As a admin
+I would like to access orange hrm portal
+
+Scenario: Validlogin
+	Given I have a browser open with OrangeHRM Website loaded
+	When I enter valid username as 'Admin'
+	And I enter valid password as 'admin123'
+	And I click on login button
+	Then I should get access to Dashboard page with 'Quick launch' text
---
+++ OpenHRM-Automation/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,40 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace OpenHRMAutomation.StepDefinitions
+{
+    [Binding]
+    public class LoginStepDefinitions
+    {
+
+        [Given(@"I have a browser open with OrangeHRM Website loaded")]
+        public void GivenIHaveABrowserOpenWithOrangeHRMWebsiteLoaded()
+        {
+            Console.WriteLine( "ORANGE HRM");
+        }
+
+        [When(@"I enter valid username as '(.*)'")]
+        public void WhenIEnterValidUsernameAs(string username)
+        {
+            Console.WriteLine( "username : "+username);
+        }
+
+        [When(@"I enter valid password as '(.*)'")]
+        public void WhenIEnterValidPasswordAs(string password)
+        {
+            Console.WriteLine("password : "+password);
+        }
+
+        [When(@"I click on login button")]
+        public void WhenIClickOnLoginButton()
+        {
+            Console.WriteLine("login button");
+        }
+
+        [Then(@"I should get access to Dashboard page with '(.*)' text")]
+        public void ThenIShouldGetAccessToDashboardPageWithText(string expectedText)
+        {
+            Console.WriteLine("Quick launch");
+        }
+    }
+}
